74aa6345bb0df4276ce38fe26bd52a3b21d6b119
dextergb <dextergb@rogers.com>
2021-06-15 20:28:57 +0000

    Practice/Explore DOM Events

---
+++ public/scripts/explore-keyboard-events.js
@@ -0,0 +1,18 @@
+/* JQuery: */
+
+// console.log("loaded");
+// $(document).ready(function () {
+//   $("#tweet-text").on("input", (event) => {
+//     console.log(event);
+//   });
+//   $("#tweet-text").on("keyup", (event) => {
+//     console.log(event);
+//   });
+// });
+
+/* Vanilla Javascript: */
+// window.onload = function () {
+//   document.getElementById("tweet-text").addEventListener("input", (event) => {
+//     console.log(event);
+//   });
+// };
