af73a8f53193a7d44fe7f90d75b8fae73e47660a
dextergb <dextergb@rogers.com>
2021-06-16 20:10:36 +0000

    Implement form submission using JQuery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -29,15 +29,30 @@ const data = [
     created_at: 1461113959088,
   },
 ];
-// This function can be responsible for taking in an array of tweet objects and then appending each one to the #tweets-container. In order to do this, the renderTweets will need to leverage the createTweetElement function you wrote earlier by passing the tweet object to it, then using the returned jQuery object by appending it to the #tweets-container section.
+
+// A page can't be manipulated safely until the document is "ready."
 $(document).ready(function () {
+  // Form submission using JQuery
+  $(".tweet-form").on("submit", function (event) {
+    event.preventDefault();
+    $.ajax({
+      method: "POST",
+      url: "http://localhost:8080/tweets",
+      data: $(this).serialize(),
+    })
+      .done(() => {
+        console.log("Posted Successfully");
+        $("#tweet-text").val("");
+      })
+      .fail((error) => console.log("Failed", error));
+  });
+
+  // This function can be responsible for taking in an array of tweet objects and then appending each one to the #tweets-container. In order to do this, the renderTweets will need to leverage the createTweetElement function you wrote earlier by passing the tweet object to it, then using the returned jQuery object by appending it to the #tweets-container section.
+
   const renderTweets = function (tweets) {
     for (let tweet in tweets) {
       $(".container").append(createTweetElement(tweets[tweet]));
     }
-    // loops through tweets
-    // calls createTweetElement for each tweet
-    // takes return value and appends it to the tweets container
   };
 
   // define a function createTweetElement that takes in a tweet object and is responsible for returning a tweet <article> element containing the entire HTML structure of the tweet.
